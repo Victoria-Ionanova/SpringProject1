fef712460748ecdd9a63dd12423bee798f3edc7e
Victoria <ionanova1.18@gmail.com>
2019-06-03 22:40:26 +0300

    задание 9 SpringFramework_Config_Log_Mapping_Validation

--- src/main/java/by/iba/ionanova/SpringProject1/controller/MainController.java
+++ src/main/java/by/iba/ionanova/SpringProject1/controller/MainController.java
@@ -2,24 +2,27 @@ package by.iba.ionanova.SpringProject1.controller;
 
 import by.iba.ionanova.SpringProject1.form.PersonForm;
 import by.iba.ionanova.SpringProject1.model.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
+@RequestMapping
 public class MainController {
 
     private static List<Person> persons = new ArrayList<Person>();
     static {
-        persons.add(new Person("Olga", "Petrova"));
-        persons.add(new Person("Nikolai", "Ivanov"));
+//        persons.add(new Person("Olga", "Petrova"));
+//        persons.add(new Person("Nikolai", "Ivanov"));
     }
     //
     // Вводится (inject) из application.properties.
@@ -29,22 +32,23 @@ public class MainController {
     @Value("${error.message}")
     private String errorMessage;
 
-    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    @GetMapping(value = {"/", "/index"})
     public ModelAndView index(Model model) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         model.addAttribute("message", message);
+        log.info("index was called");
         return modelAndView;
     }
 
-    @RequestMapping(value = {"/personList"}, method = RequestMethod.GET)
+    @GetMapping(value = {"/personList"})
     public ModelAndView personList(Model model) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("personList");
         model.addAttribute("persons", persons);
         return modelAndView;
     }
-    @RequestMapping(value = {"/addPerson"}, method = RequestMethod.GET)
+    @GetMapping(value = {"/addPerson"})
     public ModelAndView showAddPersonPage(Model model) {
         ModelAndView modelAndView = new ModelAndView("addPerson");
         PersonForm personForm = new PersonForm();
@@ -53,23 +57,30 @@ public class MainController {
     }
     // @PostMapping("/addPerson")
     //GetMapping("/")
-    @RequestMapping(value = {"/addPerson"}, method = RequestMethod.POST)
-    public ModelAndView savePerson(Model model, //
-                                   @ModelAttribute("personForm") PersonForm
-                                           personForm) {
+    @PostMapping(value = {"/addPerson"})
+    public ModelAndView savePerson( Model model,
+                                    @Valid @ModelAttribute("personForm") PersonForm personForm,
+                                    Errors errors) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("personList");
-        String firstName = personForm.getFirstName();
-        String lastName = personForm.getLastName();
-        if (firstName != null && firstName.length() > 0 //
-                && lastName != null && lastName.length() > 0) {
-            Person newPerson = new Person(firstName, lastName);
+        if (errors.hasErrors()) {
+            modelAndView.setViewName("addPerson");
+        }
+        else {
+            modelAndView.setViewName("personList");
+            String firstName = personForm.getFirstName();
+            String lastName = personForm.getLastName();
+            String street = personForm.getStreet();
+            String city = personForm.getCity();
+            String zip = personForm.getZip();
+            String email = personForm.getEmail();
+            String birthday = personForm.getBirthday();
+            Person newPerson = new Person(firstName, lastName, street, city,
+                    zip, email, birthday);
             persons.add(newPerson);
-            model.addAttribute("persons",persons);
+            model.addAttribute("persons", persons);
+            log.info("/addPerson - POST was called");
             return modelAndView;
         }
-        model.addAttribute("errorMessage", errorMessage);
-        modelAndView.setViewName("addPerson");
         return modelAndView;
     }
 
--- src/main/java/by/iba/ionanova/SpringProject1/model/Person.java
+++ src/main/java/by/iba/ionanova/SpringProject1/model/Person.java
@@ -1,31 +1,17 @@
 package by.iba.ionanova.SpringProject1.model;
 
-public class Person {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class Person {
     private String firstName;
     private String lastName;
+    private String street;
+    private String city;
+    private String zip;
+    private String email;
+    private String birthday;
 
-    public Person() {
-    }
-
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
