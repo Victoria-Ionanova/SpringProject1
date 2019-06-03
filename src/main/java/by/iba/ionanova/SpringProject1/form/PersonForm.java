ce53f205dfdf259f9db01fd980fb049fc04d4d9a
Victoria <ionanova1.18@gmail.com>
2019-06-03 21:49:43 +0300

    задание 8 SpringFramework_Введение

---
+++ src/main/java/by/iba/ionanova/SpringProject1/form/PersonForm.java
@@ -0,0 +1,20 @@
+package by.iba.ionanova.SpringProject1.form;
+
+public class PersonForm {
+
+    private String firstName;
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
